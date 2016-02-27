9cdd8bac2538e719f0661e70e52fa094c8d081ba
Tomo Masakura <masakura@codearts.co.jp>
2016-02-27 22:41:50 +0900

    テストユーザーを追加

--- FlowApp/Global.asax.cs
+++ FlowApp/Global.asax.cs
@@ -1,7 +1,11 @@
-﻿using System.Web;
+﻿using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using FlowApp.Models;
 
 namespace FlowApp
 {
@@ -13,6 +17,16 @@ namespace FlowApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Database.SetInitializer(new DbInitializer());
+
+            using (var db = new ApplicationDbContext())
+            {
+                foreach (var user in db.Users)
+                {
+                    Debug.WriteLine(user);
+                }
+            }
         }
     }
 }
---
+++ FlowApp/Models/DbInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+
+namespace FlowApp.Models
+{
+    internal sealed class DbInitializer : CreateDatabaseIfNotExists<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            base.Seed(context);
+
+            context.Users.Add(new ApplicationUser()
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "masakura@example.com",
+                Email = "masakura@example.com",
+                PasswordHash = new PasswordHasher().HashPassword("password"),
+                SecurityStamp = Guid.NewGuid().ToString()
+            });
+
+            context.SaveChanges();
+        }
+    }
+}
