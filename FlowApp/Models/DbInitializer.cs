e9a42bf1c8a52008721c3f40c2b79e0926c0746e
Tomo Masakura <masakura@codearts.co.jp>
2016-02-28 23:35:38 +0900

    ユーザーを増やした

--- FlowApp/Models/DbInitializer.cs
+++ FlowApp/Models/DbInitializer.cs
@@ -10,16 +10,25 @@ namespace FlowApp.Models
         {
             base.Seed(context);
 
-            context.Users.Add(new ApplicationUser()
-            {
-                Id = Guid.NewGuid().ToString(),
-                UserName = "masakura@example.com",
-                Email = "masakura@example.com",
-                PasswordHash = new PasswordHasher().HashPassword("password"),
-                SecurityStamp = Guid.NewGuid().ToString()
-            });
+            context.Users.Add(User("{aa45d108-5f7a-4be3-8e1e-b1e7b5453b62}", "manager1@example.com"));
+            context.Users.Add(User("{618f5ee0-c487-442c-9912-f8bd017294ea}", "manager2@example.com"));
+            context.Users.Add(User("{77d5a8b4-3ceb-41b9-9134-7f6a596f174d}", "manager3@example.com"));
+            context.Users.Add(User("{ae53b5b5-d217-44d8-89e9-d761e87028b8}", "manager4@example.com"));
+            context.Users.Add(User("{6cbfb82a-3773-4ff5-b993-f2443679ac91}", "manager5@example.com"));
 
             context.SaveChanges();
+        }
+
+        private static ApplicationUser User(string id, string userName)
+        {
+            return new ApplicationUser
+            {
+                Id = id,
+                UserName = userName,
+                Email = userName,
+                PasswordHash = new PasswordHasher().HashPassword("password"),
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
         }
     }
 }
